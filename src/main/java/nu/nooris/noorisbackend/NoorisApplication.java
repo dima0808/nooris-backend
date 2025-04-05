ef16f37da6ebc8a8f17e9a1f7b6aeadd1c5f4919
dima0808 <mamchenko2210@gmail.com>
2025-04-05 19:05:16 +0200

    init commit

---
+++ src/main/java/nu/nooris/noorisbackend/NoorisApplication.java
@@ -0,0 +1,13 @@
+package nu.nooris.noorisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoorisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoorisApplication.class, args);
+  }
+
+}
---
+++ src/test/java/nu/nooris/noorisbackend/NoorisApplicationTests.java
@@ -0,0 +1,13 @@
+package nu.nooris.noorisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoorisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
